1be89622730081e069aac6d5555e3b7b8e6c5367
wajhullah <wajhullah@ecquaria.com>
2021-12-03 09:41:17 +0800

    perform the basic range validation in the cleaned data

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,20 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+	"""
+	Check the size of the data set to be not too small and not too large
+	"""
+	assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+	"""
+	Test the price range between min_price and max_price
+	"""
+	idx = data['price'].between(min_price,max_price)
+	assert np.sum(~idx) == 0
+
+
+
+
